350199148b7ea9740d77a8b74c3b01e4d4b852fe
Molesafenetwork <owner@mole-safe.net>
2025-08-05 23:04:38 +1000

    Update oemubfocal.sh

--- oemubfocal.sh
+++ oemubfocal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Orange Pi 3B v2 POS System Setup Script
 # Compatible with Ubuntu Focal and RK3566 chipset
-# Run with: curl -sSL [your-raw-script-url] | bash
+# Run with: curl -sSL https://raw.githubusercontent.com/Molesafenetwork/msnpos2/refs/heads/main/oemubfocal.sh | bash
 
 set -e
 
