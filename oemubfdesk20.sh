140b69115d5c6308b313a33fd721fa50e80552ee
Molesafenetwork <owner@mole-safe.net>
2025-08-08 15:50:29 +1000

    Update oemubfdesk20.sh

--- oemubfdesk20.sh
+++ oemubfdesk20.sh
@@ -29,7 +29,7 @@ echo "Installing essential packages for XFCE..."
 sudo apt install -y curl git unclutter sed nano \
     wmctrl xdotool lightdm x11-utils xfce4-session \
     systemd-timesyncd openssh-server build-essential ufw \
-    xfce4-terminal xfce4-panel xfce4-settings xinit xorg
+    xfce4-terminal xfce4-panel xfce4-settings xinit xorg snapd
 
 # Install browser - try multiple options, avoid snap
 echo "Installing web browser (avoiding snap)..."
